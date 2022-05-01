e7dbf1b43d9fe165644fbe7d937cc7037b1c87f0
KiryhaVlasov <m45362388@gmail.com>
2022-05-01 19:16:10 +0300

    add project homeTask1

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт,
+// какое число большее, а какое меньшее.
+Console.WriteLine("введите первое число");
+int firstNum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите второе число");
+int secondNum = Convert.ToInt32(Console.ReadLine());
+int maxNum = 0;
+if (firstNum > secondNum )
+{
+    maxNum = firstNum; 
+}
+else
+{
+    maxNum = secondNum;
+}
+Console.WriteLine($"{maxNum} - наибольшее число");
