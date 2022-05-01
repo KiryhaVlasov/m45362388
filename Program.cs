99fbfbe618933aaef6feff927b29c04f72a24ec6
KiryhaVlasov <m45362388@gmail.com>
2022-05-01 20:17:57 +0300

    add project homeTask3

--- Program.cs
+++ Program.cs
@@ -17,23 +17,52 @@
 
 // Задача 2: Напишите программу, которая принимает на 
 // вход три числа и выдаёт максимальное из этих чисел.
-Console.WriteLine("введите первое число");
-int firstNum = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("введите второе число");
-int secondNum = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("введите третье число");
-int thirdNum = Convert.ToInt32(Console.ReadLine());
-int maxNum = 0;
-if (firstNum > secondNum & firstNum > thirdNum)
+// Console.WriteLine("введите первое число");
+// int firstNum = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите второе число");
+// int secondNum = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите третье число");
+// int thirdNum = Convert.ToInt32(Console.ReadLine());
+// int maxNum = 0;
+// if (firstNum > secondNum & firstNum > thirdNum)
+// {
+//     maxNum = firstNum; 
+// }
+// else if(secondNum > firstNum & secondNum > thirdNum)
+// {
+//     maxNum = secondNum;
+// }
+// else 
+// {
+//     maxNum = thirdNum;
+// }
+// Console.WriteLine($"{maxNum} - наибольшее число");
+
+
+// Задача 3: Напишите программу, которая на вход 
+// принимает число и выдаёт, является ли оно четным
+// Console.WriteLine("введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if (num % 2 == 0)
+// {
+//     Console.WriteLine($"число {num} четное");
+// }
+// else
+// {
+//     Console.WriteLine($"число {num } нечетное");
+// }
+
+
+
+// Задача 4  Напишите программу, которая на вход 
+// принимает число (N), а на выходе показывает все чётные 
+// числа от 1 до N.
+Console.WriteLine("Ведите число N больше 1");
+int N = Convert.ToInt32(Console.ReadLine());
+for(int i=1; i<=N; i++)
 {
-    maxNum = firstNum; 
+    if(i % 2 == 0)
+  {
+    Console.Write($"{i}  ");
+  }
 }
-else if(secondNum > firstNum & secondNum > thirdNum)
-{
-    maxNum = secondNum;
-}
-else 
-{
-    maxNum = thirdNum;
-}
-Console.WriteLine($"{maxNum} - наибольшее число");
