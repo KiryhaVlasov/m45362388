8cbd14693e0f3b2bff3bf1e23644531f6591d7dc
KiryhaVlasov <m45362388@gmail.com>
2022-05-01 19:50:09 +0300

    add project homeTask2

--- Program.cs
+++ Program.cs
@@ -1,16 +1,39 @@
 ﻿// Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт,
 // какое число большее, а какое меньшее.
+// Console.WriteLine("введите первое число");
+// int firstNum = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите второе число");
+// int secondNum = Convert.ToInt32(Console.ReadLine());
+// int maxNum = 0;
+// if (firstNum > secondNum )
+// {
+//     maxNum = firstNum; 
+// }
+// else
+// {
+//     maxNum = secondNum;
+// }
+// Console.WriteLine($"{maxNum} - наибольшее число");
+
+// Задача 2: Напишите программу, которая принимает на 
+// вход три числа и выдаёт максимальное из этих чисел.
 Console.WriteLine("введите первое число");
 int firstNum = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("введите второе число");
 int secondNum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите третье число");
+int thirdNum = Convert.ToInt32(Console.ReadLine());
 int maxNum = 0;
-if (firstNum > secondNum )
+if (firstNum > secondNum & firstNum > thirdNum)
 {
     maxNum = firstNum; 
 }
-else
+else if(secondNum > firstNum & secondNum > thirdNum)
 {
     maxNum = secondNum;
+}
+else 
+{
+    maxNum = thirdNum;
 }
 Console.WriteLine($"{maxNum} - наибольшее число");
