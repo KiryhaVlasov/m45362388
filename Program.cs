a2a8a99f3aeaaea4718a9586bc16f228995f0e2f
KiryhaVlasov <103191525+KiryhaVlasov@users.noreply.github.com>
2022-05-18 19:52:30 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,68 +1,13 @@
-﻿// Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт,
-// какое число большее, а какое меньшее.
-// Console.WriteLine("введите первое число");
-// int firstNum = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("введите второе число");
-// int secondNum = Convert.ToInt32(Console.ReadLine());
-// int maxNum = 0;
-// if (firstNum > secondNum )
-// {
-//     maxNum = firstNum; 
-// }
-// else
-// {
-//     maxNum = secondNum;
-// }
-// Console.WriteLine($"{maxNum} - наибольшее число");
-
-// Задача 2: Напишите программу, которая принимает на 
-// вход три числа и выдаёт максимальное из этих чисел.
-// Console.WriteLine("введите первое число");
-// int firstNum = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("введите второе число");
-// int secondNum = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("введите третье число");
-// int thirdNum = Convert.ToInt32(Console.ReadLine());
-// int maxNum = 0;
-// if (firstNum > secondNum & firstNum > thirdNum)
-// {
-//     maxNum = firstNum; 
-// }
-// else if(secondNum > firstNum & secondNum > thirdNum)
-// {
-//     maxNum = secondNum;
-// }
-// else 
-// {
-//     maxNum = thirdNum;
-// }
-// Console.WriteLine($"{maxNum} - наибольшее число");
-
-
-// Задача 3: Напишите программу, которая на вход 
-// принимает число и выдаёт, является ли оно четным
-// Console.WriteLine("введите число");
-// int num = Convert.ToInt32(Console.ReadLine());
-// if (num % 2 == 0)
-// {
-//     Console.WriteLine($"число {num} четное");
-// }
-// else
-// {
-//     Console.WriteLine($"число {num } нечетное");
-// }
-
-
-
-// Задача 4  Напишите программу, которая на вход 
-// принимает число (N), а на выходе показывает все чётные 
-// числа от 1 до N.
-Console.WriteLine("Ведите число N больше 1");
-int N = Convert.ToInt32(Console.ReadLine());
-for(int i=1; i<=N; i++)
-{
-    if(i % 2 == 0)
-  {
-    Console.Write($"{i}  ");
-  }
-}
+﻿//Задача 10: Напишите программу, которая принимает на
+// вход трёхзначное число и на выходе показывает вторую
+// цифру этого числа.
+// Примеры:
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+string num = Convert.ToString(number);
+num = num.Remove(0,1);
+num = num.Remove(1,1);
+Console.WriteLine(num);
